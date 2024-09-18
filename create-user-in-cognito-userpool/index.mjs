0bd9ddea9203caab59a23f2c1833cf77e87c9d24
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-09-17 21:22:50 -0300

    feat: update lambda functions to handle proxy payload from api gateway

--- create-user-in-cognito-userpool/index.mjs
+++ create-user-in-cognito-userpool/index.mjs
@@ -1,19 +1,20 @@
 import { CognitoIdentityProviderClient, AdminCreateUserCommand } from "@aws-sdk/client-cognito-identity-provider";
 
 export const handler = async (event) => {
+  const data = JSON.parse(event.body);
   const client = new CognitoIdentityProviderClient();
   const input = {
     
     UserPoolId: process.env.USER_POOL_ID,
-    Username: event.email,
+    Username: data.email,
     UserAttributes: [
       {
         Name: "email",
-        Value: event.email,
+        Value: data.email,
       },
       {
         Name: "custom:cpf",
-        Value: event.cpf,
+        Value: data.cpf,
       },
     ],
     ValidationData: [ 
--- read-user-from-cognito-userpool/index.mjs
+++ read-user-from-cognito-userpool/index.mjs
@@ -1,10 +1,11 @@
 import { CognitoIdentityProviderClient, AdminGetUserCommand } from "@aws-sdk/client-cognito-identity-provider";
 
 export const handler = async (event) => {
+  const data = JSON.parse(event.body);
   const client = new CognitoIdentityProviderClient();
   const input = {
     UserPoolId: process.env.USER_POOL_ID,
-    Username: event.username,
+    Username: data.username,
   };
   const command = new AdminGetUserCommand(input);
   const response = await client.send(command);
