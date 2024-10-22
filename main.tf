69174884781d5c0a9d3928f09858e53d21824ee1
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-10-22 18:36:11 -0300

    feat: include s3_bucket_public_access_block and terraform.lock

--- main.tf
+++ main.tf
@@ -137,3 +137,11 @@ resource "aws_lambda_permission" "apigw" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
+  bucket = aws_s3_bucket.example.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
